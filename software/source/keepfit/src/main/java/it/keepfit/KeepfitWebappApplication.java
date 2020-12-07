8ce52195464ca51b0ab8641c0ed141d396a618bd
Vittorio Libretti <vittorio.libretti.ing@gmail.com>
2020-12-07 12:13:16 +0100

    Piano di test e codice sorgente

---
+++ software/source/keepfit/src/main/java/it/keepfit/KeepfitWebappApplication.java
@@ -0,0 +1,13 @@
+package it.keepfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepfitWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepfitWebappApplication.class, args);
+	}
+
+}
---
+++ software/source/keepfit/src/test/java/it/keepfit/KeepfitWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package it.keepfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepfitWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
